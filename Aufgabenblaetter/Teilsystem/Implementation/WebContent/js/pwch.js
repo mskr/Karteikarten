8beed3a0460bb0c4f9feb251dfb77d5657efc7ac
fs79 <fabian.schilha@uni-ulm.de>
2015-01-17 14:02:36 +0100

    neues PW und wiederholung neues PW Prüfung added

--- Aufgabenblaetter/Teilsystem/Implementation/WebContent/js/pwch.js
+++ Aufgabenblaetter/Teilsystem/Implementation/WebContent/js/pwch.js
@@ -1,7 +1,16 @@
 
 function subnewpw(){
-	document.getElementById('action').value = 'changePW';
-	document.getElementById('oldhashpw').value = MD5(document.getElementById('oldPassword').value)
 	document.getElementById('newhashpw').value = MD5(document.getElementById('NewPw').value);
 	document.getElementById('newrephashpw').value = MD5(document.getElementById('repNewPw').value);
+	
+	if (document.getElementById('newhashpw').value != document.getElementById('newrephashpw').value){
+		alert("Neues Kennwort und wiederholtes Passwort sind unterschiedlich! Bitte geben Sie diese erneut ein!");
+		document.getElementById('action').value = 'changePW';
+	}
+		  
+	if (document.getElementById('newhashpw').value == document.getElementById('newrephashpw').value){
+		document.getElementById('action').value = 'changePW';
+		document.getElementById('oldhashpw').value = MD5(document.getElementById('oldPassword').value);
+	}
+		
 }
