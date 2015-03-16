ffe22e23bb06a85eb98ebfac8a17f8f635f30aba
Matthias <Matthias@MatthiasLen-PC.mshome.net>
2015-03-16 14:16:32 +0100

    neo4j jdbc driver hinzugefügt mysql datenbank exportiert

---
+++ AKTUELL/prog/install/sopra.sql
@@ -0,0 +1,456 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 16. Mrz 2015 um 11:08
+-- Server Version: 5.6.21
+-- PHP-Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Datenbank: `sopra`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `benutzer`
+--
+
+CREATE TABLE IF NOT EXISTS `benutzer` (
+  `eMail` varchar(30) NOT NULL,
+  `Vorname` varchar(30) NOT NULL,
+  `Nachname` varchar(30) NOT NULL,
+  `Matrikelnummer` int(11) NOT NULL,
+  `Studiengang` varchar(30) NOT NULL,
+  `Kennwort` varchar(30) NOT NULL,
+  `Nutzerstatus` enum('Benutzer','Dozent','Admin','') NOT NULL,
+  `NotifyDisussionen` enum('KEINE','VERANSTALTUNG_TEILGENOMMEN','DISKUSSION_TEILGENOMMEN','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `benutzer_veranstaltung_zuordnung`
+--
+
+CREATE TABLE IF NOT EXISTS `benutzer_veranstaltung_zuordnung` (
+`ID` int(11) NOT NULL,
+  `Benutzer` varchar(30) NOT NULL,
+  `Veranstaltung` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `bewertung_karteikarte`
+--
+
+CREATE TABLE IF NOT EXISTS `bewertung_karteikarte` (
+`ID` int(11) NOT NULL,
+  `Bewertung` int(11) NOT NULL,
+  `Benutzer` varchar(30) NOT NULL,
+  `KarteikarteID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `bewertung_kommentar`
+--
+
+CREATE TABLE IF NOT EXISTS `bewertung_kommentar` (
+`ID` int(11) NOT NULL,
+  `Bewertung` int(11) NOT NULL,
+  `Benutzer` varchar(30) NOT NULL,
+  `KommentarID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `einladung_moderator_benachrichtigung`
+--
+
+CREATE TABLE IF NOT EXISTS `einladung_moderator_benachrichtigung` (
+`ID` int(11) NOT NULL,
+  `Inhalt` text NOT NULL,
+  `Erstelldatum` date NOT NULL,
+  `Benutzer` varchar(30) NOT NULL,
+  `Veranstaltung` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `karteikarte`
+--
+
+CREATE TABLE IF NOT EXISTS `karteikarte` (
+`ID` int(11) NOT NULL,
+  `Titel` text NOT NULL,
+  `Typ` enum('Text','Bild','Video','') NOT NULL,
+  `Bewertung` int(11) NOT NULL,
+  `Aenderungsdatum` date NOT NULL,
+  `Veranstaltung` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `karteikarten_struktur`
+--
+
+CREATE TABLE IF NOT EXISTS `karteikarten_struktur` (
+`ID` int(11) NOT NULL,
+  `Position` int(11) NOT NULL,
+  `SohnKarteik` int(11) NOT NULL,
+  `VaterKarteik` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `kommentar`
+--
+
+CREATE TABLE IF NOT EXISTS `kommentar` (
+`ID` int(11) NOT NULL,
+  `Inhalt` text NOT NULL,
+  `Erstelldatum` date NOT NULL,
+  `Bewertung` int(11) NOT NULL,
+  `Benutzer` varchar(30) NOT NULL,
+  `KarteikarteID` int(11) NOT NULL,
+  `VaterkommentarID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `moderator`
+--
+
+CREATE TABLE IF NOT EXISTS `moderator` (
+`ID` int(11) NOT NULL,
+  `Benutzer` varchar(30) NOT NULL,
+  `Veranstaltung` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `notiz`
+--
+
+CREATE TABLE IF NOT EXISTS `notiz` (
+`ID` int(11) NOT NULL,
+  `Inhalt` text NOT NULL,
+  `Benutzer` varchar(30) NOT NULL,
+  `KarteikarteID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `studiengang`
+--
+
+CREATE TABLE IF NOT EXISTS `studiengang` (
+  `Name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `veranstaltung`
+--
+
+CREATE TABLE IF NOT EXISTS `veranstaltung` (
+  `Beschreibung` text NOT NULL,
+  `Semester` text NOT NULL,
+  `Kennwort` text NOT NULL,
+  `DisskusionErlaubt` tinyint(1) NOT NULL,
+  `BewertungenErlaubt` tinyint(1) NOT NULL,
+  `ModeratorKarteikartenBearbeiten` tinyint(1) NOT NULL,
+  `Ersteller` varchar(30) NOT NULL,
+  `Titel` varchar(30) NOT NULL,
+  `ErsteKarteik` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `veranstaltungs_benachrichtigung`
+--
+
+CREATE TABLE IF NOT EXISTS `veranstaltungs_benachrichtigung` (
+`ID` int(11) NOT NULL,
+  `Inhalt` text NOT NULL,
+  `Erstelldatum` date NOT NULL,
+  `Veranstaltung` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `veranstaltung_studiengang_zuordnung`
+--
+
+CREATE TABLE IF NOT EXISTS `veranstaltung_studiengang_zuordnung` (
+`ID` int(11) NOT NULL,
+  `Veranstaltung` varchar(30) NOT NULL,
+  `Studiengang` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `benutzer`
+--
+ALTER TABLE `benutzer`
+ ADD PRIMARY KEY (`eMail`), ADD KEY `Studiengang` (`Studiengang`);
+
+--
+-- Indizes für die Tabelle `benutzer_veranstaltung_zuordnung`
+--
+ALTER TABLE `benutzer_veranstaltung_zuordnung`
+ ADD PRIMARY KEY (`ID`), ADD KEY `BenutzerID` (`Benutzer`), ADD KEY `VeranstaltungID` (`Veranstaltung`), ADD KEY `VeranstaltungID_2` (`Veranstaltung`);
+
+--
+-- Indizes für die Tabelle `bewertung_karteikarte`
+--
+ALTER TABLE `bewertung_karteikarte`
+ ADD PRIMARY KEY (`ID`), ADD KEY `BenutzerID` (`Benutzer`), ADD KEY `KarteikarteID` (`KarteikarteID`);
+
+--
+-- Indizes für die Tabelle `bewertung_kommentar`
+--
+ALTER TABLE `bewertung_kommentar`
+ ADD PRIMARY KEY (`ID`), ADD KEY `BenutzerID` (`Benutzer`), ADD KEY `KommentarID` (`KommentarID`);
+
+--
+-- Indizes für die Tabelle `einladung_moderator_benachrichtigung`
+--
+ALTER TABLE `einladung_moderator_benachrichtigung`
+ ADD PRIMARY KEY (`ID`), ADD KEY `Benutzer` (`Benutzer`), ADD KEY `Veranstaltung` (`Veranstaltung`);
+
+--
+-- Indizes für die Tabelle `karteikarte`
+--
+ALTER TABLE `karteikarte`
+ ADD PRIMARY KEY (`ID`), ADD KEY `VeranstaltungID` (`Veranstaltung`);
+
+--
+-- Indizes für die Tabelle `karteikarten_struktur`
+--
+ALTER TABLE `karteikarten_struktur`
+ ADD PRIMARY KEY (`ID`), ADD KEY `SohnKarteik` (`SohnKarteik`), ADD KEY `VaterKarteik` (`VaterKarteik`);
+
+--
+-- Indizes für die Tabelle `kommentar`
+--
+ALTER TABLE `kommentar`
+ ADD PRIMARY KEY (`ID`), ADD KEY `ErstellerID` (`Benutzer`), ADD KEY `DiskussionID` (`KarteikarteID`), ADD KEY `VaterkommentarID` (`VaterkommentarID`);
+
+--
+-- Indizes für die Tabelle `moderator`
+--
+ALTER TABLE `moderator`
+ ADD PRIMARY KEY (`ID`), ADD KEY `Benutzer` (`Benutzer`), ADD KEY `Veranstaltung` (`Veranstaltung`);
+
+--
+-- Indizes für die Tabelle `notiz`
+--
+ALTER TABLE `notiz`
+ ADD PRIMARY KEY (`ID`), ADD KEY `BenutzerID` (`Benutzer`), ADD KEY `KarteikarteID` (`KarteikarteID`);
+
+--
+-- Indizes für die Tabelle `studiengang`
+--
+ALTER TABLE `studiengang`
+ ADD PRIMARY KEY (`Name`);
+
+--
+-- Indizes für die Tabelle `veranstaltung`
+--
+ALTER TABLE `veranstaltung`
+ ADD PRIMARY KEY (`Titel`), ADD KEY `Ersteller` (`Ersteller`), ADD KEY `ErsteKarteik` (`ErsteKarteik`);
+
+--
+-- Indizes für die Tabelle `veranstaltungs_benachrichtigung`
+--
+ALTER TABLE `veranstaltungs_benachrichtigung`
+ ADD PRIMARY KEY (`ID`), ADD KEY `VeranstaltungID` (`Veranstaltung`);
+
+--
+-- Indizes für die Tabelle `veranstaltung_studiengang_zuordnung`
+--
+ALTER TABLE `veranstaltung_studiengang_zuordnung`
+ ADD PRIMARY KEY (`ID`), ADD KEY `Veranstaltung` (`Veranstaltung`), ADD KEY `Studiengang` (`Studiengang`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `benutzer_veranstaltung_zuordnung`
+--
+ALTER TABLE `benutzer_veranstaltung_zuordnung`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `bewertung_karteikarte`
+--
+ALTER TABLE `bewertung_karteikarte`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `bewertung_kommentar`
+--
+ALTER TABLE `bewertung_kommentar`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `einladung_moderator_benachrichtigung`
+--
+ALTER TABLE `einladung_moderator_benachrichtigung`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `karteikarte`
+--
+ALTER TABLE `karteikarte`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `karteikarten_struktur`
+--
+ALTER TABLE `karteikarten_struktur`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `kommentar`
+--
+ALTER TABLE `kommentar`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `moderator`
+--
+ALTER TABLE `moderator`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `notiz`
+--
+ALTER TABLE `notiz`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `veranstaltungs_benachrichtigung`
+--
+ALTER TABLE `veranstaltungs_benachrichtigung`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `veranstaltung_studiengang_zuordnung`
+--
+ALTER TABLE `veranstaltung_studiengang_zuordnung`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `benutzer`
+--
+ALTER TABLE `benutzer`
+ADD CONSTRAINT `benutzer_ibfk_1` FOREIGN KEY (`Studiengang`) REFERENCES `studiengang` (`Name`);
+
+--
+-- Constraints der Tabelle `benutzer_veranstaltung_zuordnung`
+--
+ALTER TABLE `benutzer_veranstaltung_zuordnung`
+ADD CONSTRAINT `benutzer_veranstaltung_zuordnung_ibfk_1` FOREIGN KEY (`Veranstaltung`) REFERENCES `veranstaltung` (`Titel`),
+ADD CONSTRAINT `benutzer_veranstaltung_zuordnung_ibfk_2` FOREIGN KEY (`Benutzer`) REFERENCES `benutzer` (`eMail`);
+
+--
+-- Constraints der Tabelle `bewertung_karteikarte`
+--
+ALTER TABLE `bewertung_karteikarte`
+ADD CONSTRAINT `bewertung_karteikarte_ibfk_2` FOREIGN KEY (`KarteikarteID`) REFERENCES `karteikarte` (`ID`),
+ADD CONSTRAINT `bewertung_karteikarte_ibfk_3` FOREIGN KEY (`Benutzer`) REFERENCES `benutzer` (`eMail`);
+
+--
+-- Constraints der Tabelle `bewertung_kommentar`
+--
+ALTER TABLE `bewertung_kommentar`
+ADD CONSTRAINT `bewertung_kommentar_ibfk_2` FOREIGN KEY (`KommentarID`) REFERENCES `kommentar` (`ID`),
+ADD CONSTRAINT `bewertung_kommentar_ibfk_3` FOREIGN KEY (`Benutzer`) REFERENCES `benutzer` (`eMail`);
+
+--
+-- Constraints der Tabelle `einladung_moderator_benachrichtigung`
+--
+ALTER TABLE `einladung_moderator_benachrichtigung`
+ADD CONSTRAINT `einladung_moderator_benachrichtigung_ibfk_1` FOREIGN KEY (`Benutzer`) REFERENCES `benutzer` (`eMail`),
+ADD CONSTRAINT `einladung_moderator_benachrichtigung_ibfk_2` FOREIGN KEY (`Veranstaltung`) REFERENCES `veranstaltung` (`Titel`);
+
+--
+-- Constraints der Tabelle `karteikarte`
+--
+ALTER TABLE `karteikarte`
+ADD CONSTRAINT `karteikarte_ibfk_3` FOREIGN KEY (`Veranstaltung`) REFERENCES `veranstaltung` (`Titel`);
+
+--
+-- Constraints der Tabelle `karteikarten_struktur`
+--
+ALTER TABLE `karteikarten_struktur`
+ADD CONSTRAINT `karteikarten_struktur_ibfk_1` FOREIGN KEY (`SohnKarteik`) REFERENCES `karteikarte` (`ID`),
+ADD CONSTRAINT `karteikarten_struktur_ibfk_2` FOREIGN KEY (`VaterKarteik`) REFERENCES `karteikarte` (`ID`);
+
+--
+-- Constraints der Tabelle `kommentar`
+--
+ALTER TABLE `kommentar`
+ADD CONSTRAINT `kommentar_ibfk_4` FOREIGN KEY (`KarteikarteID`) REFERENCES `karteikarte` (`ID`),
+ADD CONSTRAINT `kommentar_ibfk_5` FOREIGN KEY (`VaterkommentarID`) REFERENCES `kommentar` (`ID`) ON DELETE CASCADE,
+ADD CONSTRAINT `kommentar_ibfk_6` FOREIGN KEY (`Benutzer`) REFERENCES `benutzer` (`eMail`);
+
+--
+-- Constraints der Tabelle `moderator`
+--
+ALTER TABLE `moderator`
+ADD CONSTRAINT `moderator_ibfk_1` FOREIGN KEY (`Benutzer`) REFERENCES `benutzer` (`eMail`),
+ADD CONSTRAINT `moderator_ibfk_2` FOREIGN KEY (`Veranstaltung`) REFERENCES `veranstaltung` (`Titel`);
+
+--
+-- Constraints der Tabelle `notiz`
+--
+ALTER TABLE `notiz`
+ADD CONSTRAINT `notiz_ibfk_2` FOREIGN KEY (`KarteikarteID`) REFERENCES `karteikarte` (`ID`),
+ADD CONSTRAINT `notiz_ibfk_3` FOREIGN KEY (`Benutzer`) REFERENCES `benutzer` (`eMail`);
+
+--
+-- Constraints der Tabelle `veranstaltung`
+--
+ALTER TABLE `veranstaltung`
+ADD CONSTRAINT `veranstaltung_ibfk_1` FOREIGN KEY (`Ersteller`) REFERENCES `benutzer` (`eMail`),
+ADD CONSTRAINT `veranstaltung_ibfk_2` FOREIGN KEY (`ErsteKarteik`) REFERENCES `karteikarte` (`ID`);
+
+--
+-- Constraints der Tabelle `veranstaltungs_benachrichtigung`
+--
+ALTER TABLE `veranstaltungs_benachrichtigung`
+ADD CONSTRAINT `veranstaltungs_benachrichtigung_ibfk_2` FOREIGN KEY (`Veranstaltung`) REFERENCES `veranstaltung` (`Titel`);
+
+--
+-- Constraints der Tabelle `veranstaltung_studiengang_zuordnung`
+--
+ALTER TABLE `veranstaltung_studiengang_zuordnung`
+ADD CONSTRAINT `veranstaltung_studiengang_zuordnung_ibfk_1` FOREIGN KEY (`Veranstaltung`) REFERENCES `veranstaltung` (`Titel`),
+ADD CONSTRAINT `veranstaltung_studiengang_zuordnung_ibfk_2` FOREIGN KEY (`Studiengang`) REFERENCES `studiengang` (`Name`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
