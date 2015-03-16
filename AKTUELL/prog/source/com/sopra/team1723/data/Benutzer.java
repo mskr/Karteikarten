3f3545ea6600d28739f9246b51764ca55d07bd3b
Matthias <Matthias@MatthiasLen-PC.mshome.net>
2015-03-16 16:27:34 +0100

    Datenbankmethoden für Login und Registrierung Benutzereinstellungen werden nichtmehr ausgelagert sondern direkt im Benutzer gespeichert

--- AKTUELL/prog/source/com/sopra/team1723/data/Benutzer.java
+++ AKTUELL/prog/source/com/sopra/team1723/data/Benutzer.java
@@ -12,11 +12,123 @@ public class Benutzer {
      */
     public Benutzer() {
     }
+    
+    public Benutzer(String eMail, String vorname, String nachname, int matrikelnummer, String studiengang,
+            String kennwort, Nutzerstatus nutzerstatus, boolean notifyVeranstAenderung,
+            boolean notifyKarteikartenAenderung, NotifyKommentare notifyKommentare)
+    {
+        this.eMail = eMail;
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.matrikelnummer = matrikelnummer;
+        this.studiengang = studiengang;
+        this.kennwort = kennwort;
+        this.nutzerstatus = nutzerstatus;
+        this.notifyVeranstAenderung = notifyVeranstAenderung;
+        this.notifyKarteikartenAenderung = notifyKarteikartenAenderung;
+        this.notifyKommentare = notifyKommentare;
+    }
 
     /**
      * 
      */
     private String eMail;
+
+    public String geteMail()
+    {
+        return eMail;
+    }
+
+
+    public String getVorname()
+    {
+        return vorname;
+    }
+
+    public void setVorname(String vorname)
+    {
+        this.vorname = vorname;
+    }
+
+    public String getNachname()
+    {
+        return nachname;
+    }
+
+    public void setNachname(String nachname)
+    {
+        this.nachname = nachname;
+    }
+
+    public int getMatrikelnummer()
+    {
+        return matrikelnummer;
+    }
+
+    public boolean isNotifyVeranstAenderung()
+    {
+        return notifyVeranstAenderung;
+    }
+
+    public void setNotifyVeranstAenderung(boolean notifyVeranstAenderung)
+    {
+        this.notifyVeranstAenderung = notifyVeranstAenderung;
+    }
+
+    public boolean isNotifyKarteikartenAenderung()
+    {
+        return notifyKarteikartenAenderung;
+    }
+
+    public void setNotifyKarteikartenAenderung(boolean notifyKarteikartenAenderung)
+    {
+        this.notifyKarteikartenAenderung = notifyKarteikartenAenderung;
+    }
+
+    public NotifyKommentare getNotifyKommentare()
+    {
+        return notifyKommentare;
+    }
+
+    public void setNotifyKommentare(NotifyKommentare notifyKommentare)
+    {
+        this.notifyKommentare = notifyKommentare;
+    }
+
+    public void setMatrikelnummer(int matrikelnummer)
+    {
+        this.matrikelnummer = matrikelnummer;
+    }
+
+    public String getStudiengang()
+    {
+        return studiengang;
+    }
+
+    public void setStudiengang(String studiengang)
+    {
+        this.studiengang = studiengang;
+    }
+
+    public String getKennwort()
+    {
+        return kennwort;
+    }
+
+    public void setKennwort(String kennwort)
+    {
+        this.kennwort = kennwort;
+    }
+
+    public Nutzerstatus getNutzerstatus()
+    {
+        return nutzerstatus;
+    }
+
+    public void setNutzerstatus(Nutzerstatus nutzerstatus)
+    {
+        this.nutzerstatus = nutzerstatus;
+    }
 
     /**
      * 
@@ -31,7 +143,7 @@ public class Benutzer {
     /**
      * 
      */
-    private int martrikelnummer;
+    private int matrikelnummer;
 
     /**
      * 
@@ -47,5 +159,21 @@ public class Benutzer {
      * 
      */
     private Nutzerstatus nutzerstatus;
+   
+
+    /**
+     * 
+     */
+    private boolean notifyVeranstAenderung;
+
+    /**
+     * 
+     */
+    private boolean notifyKarteikartenAenderung;
+
+    /**
+     * 
+     */
+    private NotifyKommentare notifyKommentare;
 
 }
