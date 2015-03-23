64c811cd0e6d02df1d48fa39d347cecd21b1d4af
Andreas Rottach <Andreas@Andi-PC>
2015-03-23 11:02:54 +0100

    Profil im Admin-Modus speichern funktioniert.

--- AKTUELL/prog/source/com/sopra/team1723/ctrl/BenutzerServlet.java
+++ AKTUELL/prog/source/com/sopra/team1723/ctrl/BenutzerServlet.java
@@ -137,7 +137,7 @@ public class BenutzerServlet extends ServletController {
         String studienGang = request.getParameter(requestStudiengang);
         String matrikelNrStr = request.getParameter(requestMatrikelNr);
         
-        if(passwort.contains(" ") || isEmptyAndRemoveSpaces(email) || isEmptyAndRemoveSpaces(passwort) || isEmptyAndRemoveSpaces(vorname)||isEmptyAndRemoveSpaces(nachname)
+        if(isEmptyAndRemoveSpaces(email) || isEmpty(passwort) || isEmptyAndRemoveSpaces(vorname)||isEmptyAndRemoveSpaces(nachname)
                 || isEmptyAndRemoveSpaces(studienGang) || isEmptyAndRemoveSpaces(matrikelNrStr))
         {
             jo = JSONConverter.toJsonError(JSONConverter.jsonErrorInvalidParam);
