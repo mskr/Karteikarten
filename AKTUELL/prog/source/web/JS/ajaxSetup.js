78f784dbc86f80a03ede34614878b8b026bcd8be
Andreas Rottach <andreas.rottach@aol.com>
2015-05-05 18:13:25 +0200

    Verbindungsaufbau wird jetzt gut gehänelt. Wenn die Verbindung wieder hergestellt wurde, wird die Seite neu geladen.

--- AKTUELL/prog/source/web/JS/ajaxSetup.js
+++ AKTUELL/prog/source/web/JS/ajaxSetup.js
@@ -8,22 +8,47 @@
  * wird ein Fehler ausgegeben.
  * Setzt einen Timeout.
  */
+
 $.ajaxSetup({
     type: "POST",
 	timeout: 3000,
 	error: function(jqXHR, textStatus, errorThrown) { 
-	    //status === 'timeout' if it took too long.
-	    //handle that however you want.
-		
 		if(textStatus == "timeout")
+		{
 		    showError("Verbindung zum Server wurde unterbrochen. Wiederholen Sie den Vorngang, wenn möglich.");
+			checkConnection();
+		}
 		else if(jqXHR.responseText == "")
-		    showError("Bitte prüfen Sie Ihre Internetverbindung! Es konnte keine Verbindung zum Server hergestellt werden.");
+		{
+		    showError("Bitte prüfen Sie Ihre Internetverbindung! Es konnte keine Verbindung zum Server hergestellt werden. Versuche Verbindung wieder herzustellen...");
+			checkConnection();
+		}
 		else
-			showError("Ajax Call returned error. Debug Info: "+jqXHR.status+", "+textStatus+", "+errorThrown + "resonseText: " + jqXHR.responseText);
+			showError("Unbekannter Fehler. AjaxStatus: "+jqXHR.status+", AjaxTextStatus: "+textStatus+", AjaxErrorCode: "+errorThrown + ", resonseText: " + jqXHR.responseText);
 	}
 });
 
+function checkConnection() 
+{
+	$.ajax({
+        url: startseitenServlet,
+        data: "action="+actionGetSemester,
+        success: function(jsonResponse) {
+		    showInfo("Verbindungsaufbau erfolgreich. Bitte warten...");
+		    setTimeout(function(){
+		    	location.reload();
+		    }, 3000);
+        },
+        error:function(){
+        	setTimeout(function() { 
+    		    showError("Verbindungsaufbau fehlgeschlagen. Versuche Verbindung wieder herzustellen...");
+    		    setTimeout(function(){
+        			checkConnection();
+    		    }, 3000);
+        	}, 5000);
+        }
+    });
+}
 
 $( document ).ajaxStart(function() {
 	  $("#loadingDiv").fadeIn();
