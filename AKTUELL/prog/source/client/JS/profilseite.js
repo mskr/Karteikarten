4aab25255e74cc0e993fa122e1726568f4188edb
Marius <marius_kircher@web.de>
2015-07-05 22:55:09 +0200

    urlHandler kommentiert

--- AKTUELL/prog/source/client/JS/profilseite.js
+++ AKTUELL/prog/source/client/JS/profilseite.js
@@ -131,7 +131,7 @@ function fillProfilseite()
     
     if(currentProfilID == jsonBenutzer[paramId])
     {
-    	UserName = jsonBenutzer[paramVorname]+" "+jsonBenutzer[paramNachname];
+    	var UserName = jsonBenutzer[paramVorname]+" "+jsonBenutzer[paramNachname];
     	document.title = UserName;
     	fillMyProfil(jsonBenutzer[paramNutzerstatus] == "ADMIN");
     }
@@ -264,7 +264,7 @@ function fillMyProfil(isAdmin)
 function fillOtherProfil(benutzer, isAdmin)
 {
 	//aktualisiere Titel
-	UserName = benutzer[paramVorname]+" "+benutzer[paramNachname];
+	var UserName = benutzer[paramVorname]+" "+benutzer[paramNachname];
 	document.title = UserName;
 
 	$("#profil_avatar_loeschen_bt").hide();
@@ -381,6 +381,7 @@ function registerProfilSpeichernEvents() {
         params[paramNotifyKarteikartenAenderung] = notifyKarteikarten;
         params[paramNotifyKommentare] = notifyKommentare;
         params[paramEmailNew] = email;
+        params[paramTheme] = jsonBenutzer[paramTheme];
         if(jsonBenutzer[paramNutzerstatus] == "ADMIN")
         {
             params[paramNutzerstatus] = rolle;
--- AKTUELL/prog/source/client/JS/urlHandler.js
+++ AKTUELL/prog/source/client/JS/urlHandler.js
@@ -1,4 +1,4 @@
-/**
+/*
  * Wird beim Neuladen der Seite automatisch aktiv:
  * 1. Liest die URL Parameter
  * 2. Sendet action getBenutzer an Server
@@ -7,8 +7,16 @@
  * @author Marius, Andreas
  */
 
-// URL mit der die Seite geladen wurde
+/**
+ * Speichere global die URL mit der die Seite geladen wurde
+ */
 var initialURL="";
+
+/**
+ * Speichere global den aktuell eingeloggten Benutzer als JSON Objekt
+ */
+var jsonBenutzer;
+
 $(document).ready(function() {
 	
 	initialURL = History.getState().url;
@@ -17,12 +25,14 @@ $(document).ready(function() {
 	$(".mainbox").hide();
 	$(".mypersonalbox").hide();
 	
+	// Pruefe, ob ein Benutzer eingeloggt ist, und wenn ja welcher
     $.when(getBenutzer()).done(function(a1) {
     	var firstRunFinished = false;
     	// zu Beginn warten bis ALLE Ajax-Calls fertig sind
     	$( document ).ajaxStop(function() {
     		if(!firstRunFinished)
     		{
+    		    // Wurde die Seite mit URL Parametern aufgerufen, entscheide was angezeigt werden soll
 				var urlQuery = parseUrlQuery();
 				interpreteUrlQuery(urlQuery);
 				firstRunFinished = true;
@@ -31,70 +41,11 @@ $(document).ready(function() {
     });
     
     // Auf zurueck und vorwaerts im Browser reagieren
-    History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
+    History.Adapter.bind(window, "statechange", function() {
         var urlQuery = parseUrlQuery();
         interpreteUrlQuery(urlQuery);
     });
 });
-
-/**
- * Wird bei Groesssenaenderung des Fensters sowie bei Seitenwechseln aufgerufen.
- * Blendet kontextsensitive Buttons in der Ansicht fuer kleine Bildschirme ein bzw. aus.
- */
-var onResizeHandler = function(){
-	// Elemente fuer kleine Bildschirme
-    if (window.matchMedia("(max-width: 56em)").matches)
-    {
-    	if(window.location.href.indexOf("hauptseite")>=0){
-    		$(".r-suche_etwas_label").show();
-    		$(".r-kk-inhaltsvz-toggle").hide();
-            $(".r-kk-inhaltsvz-toggle-screensize-m").hide();
-    	}
-    	else if(window.location.href.indexOf("veranstaltungsseite")>=0){
-    	    $(".r-kk-inhaltsvz-toggle-screensize-m").removeAttr("style");
-    		$(".r-kk-inhaltsvz-toggle").removeAttr("style");
-    		$(".r-suche_etwas_label").hide();
-    	}
-    	else
-        {
-            $(".r-suche_etwas_label").hide();
-            $(".r-kk-inhaltsvz-toggle").hide();
-            $(".r-kk-inhaltsvz-toggle-screensize-m").hide();
-        }
-    }
-    else if (window.matchMedia("(max-width: 80em)").matches)
-    {
-        if(window.location.href.indexOf("hauptseite")>=0){
-            $(".r-suche_etwas_label").hide();
-            $(".r-kk-inhaltsvz-toggle").hide();
-            $(".r-kk-inhaltsvz-toggle-screensize-m").hide();
-        }
-        else if(window.location.href.indexOf("veranstaltungsseite")>=0){
-            $(".r-kk-inhaltsvz-toggle-screensize-m").removeAttr("style");
-            $(".r-kk-inhaltsvz-toggle").hide();
-            $(".r-suche_etwas_label").hide();
-        }
-        else
-        {
-            $(".r-suche_etwas_label").hide();
-            $(".r-kk-inhaltsvz-toggle").hide();
-            $(".r-kk-inhaltsvz-toggle-screensize-m").hide();
-        }
-    }
-    else
-    {
-        $(".r-suche_etwas_label").hide();
-        $(".r-kk-inhaltsvz-toggle").hide();
-        $(".r-kk-inhaltsvz-toggle-screensize-m").hide();
-    }
-}
-
-$( window ).resize(function() {    
-	onResizeHandler();
-});
-
-//Der aktuell eingeloggte Benutzer als JSON Objekt.
-var jsonBenutzer;
 
 /**
  * Packt alle Parameter aus der URL in ein Objekt.
@@ -103,99 +54,117 @@ var jsonBenutzer;
  */
 function parseUrlQuery(qry) {
     var match,
-    pl     = /\+/g,  // Regex for replacing addition symbol with a space
+    pl     = /\+/g,
     search = /([^&=]+)=?([^&]*)/g,
     decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
     query  = window.location.search.substring(1),
     urlParams = {};
-    
     if(qry != undefined)
     	query = qry;
-    
-    while (match = search.exec(query)) {
+    while (match = search.exec(query))
+    {
         urlParams[decode(match[1])] = decode(match[2]);
     }
     return urlParams;
 }
 
 /**
- * Liefert den Wert zu einem gegebenen Parameternamen.
- * @see http://stackoverflow.com/questions/901115
- * @param name ist der Parametername
- * @returns Wert des Parameters
- * oder leerer String bei nicht vorhandenem Parameter
- */
-function getUrlParameterByName(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-
-/**
+ * Haengt einen Parameter String an die URL an ohne die Seite neu zu laden.
+ * Das erste Element in paramObj muss immer location sein
+ * und einen Wert aus dem Array alleAnsichten enthalten.
  * Wird aufgerufen wenn die Ansicht gewechselt wird.
- * Das erste Element in paramObj muss immer location sein.
- * Dieses gibt den Index der Ansicht im Array alleAnsichten an.
- * HÃ¤ngt den Query String an die URL an.
- * Danach wird die Seite automatisch neu geladen.
  * @param paramObj enthaelt alle Query Parameter
  */
 function buildUrlQuery(paramObj) 
 {
     var locationSearchTmp = "?";
+    // Zaehler dienen dazu kein '&' nach dem letzten Paremeter zu schreiben
     var i = 0;
-    // Anzahl elemente Bestimmen
     var maxI = 0;
     for(var param in paramObj) 
     {
     	maxI++;
     }
-    
     for(var param in paramObj) 
     {
+        // Schreibe alle Parameter aus dem gegebenen Objekt in einen String
         locationSearchTmp += param + "=" + paramObj[param]
-
         if(i < maxI-1)
         	locationSearchTmp += "&";
-        
         i++;
     }
-    
-    // TODO
-    //location.search = locationSearchTmp; // Dies laedt auch die Seite neu
-    var State =  History.getState();
-    History.pushState(null,null, locationSearchTmp);
-    
-    // Beim Seitenwechsel alle Nachrichten verwerfen
-    // TODO Ist das sinvoll?
-    //clearMessageQueue();
-
-    // WORKAROUND Wenn zeilseite = aktuelle seite, dann wird die Change Funktion nicht getriggert
+    // Neuen Eintrag in den Browserverlauf pushen
+    History.pushState(null, null, locationSearchTmp);
+    // Pruefe, ob der String nun auch wirklich in der URL steht
+    var State = History.getState();
     if(State.url.indexOf(locationSearchTmp) >= 0)
     {
+        // Wenn ja dann starte die Anzeige der entprechenden Ansicht
     	var urlQuery = parseUrlQuery(undefined);
         interpreteUrlQuery(urlQuery);
     }
 }
 
 /**
+ * Interpretiert die aktuell in der URL stehenden Parameter.
+ * Gibt der location-Parameter eine gueltige Ansicht an, wird diese gezeigt.
+ * Dies entpricht dem Laden einer neuen HTML Seite bei klassischen Webseiten.
  * Wird aufgerufen wenn die getBenutzer Anfrage vom Server beantwortet wurde.
- * Interpretiert den URL Query String.
- * Aktuell wird nur der location Parameter gelesen und
- * die entsprechende Seite angezeigt.
- * @param paramObj enthaehlt die Parameter als Map
+ * @param paramObj enthaehlt die Parameter als Key-Value Paare
  */
-function interpreteUrlQuery(paramObj) 
+function interpreteUrlQuery(paramObj)
 {	
-	
-	onResizeHandler();
-	// Alle Waypoints deaktivieren
-    Waypoint.destroyAll();
-	
+    // location lesen
     var ziel = paramObj[urlParamLocation];
-	// Benutzer eingeloggt
-    if(jsonBenutzer != undefined)
-    { 
+	// Pruefe ob ein Benutzer eingeloggt ist
+    if(jsonBenutzer != undefined) // Ein Benutzer ist eingeloggt
+    {
+        // Lade zuerst die mypersonalbox wie in mypersonalbox.js definiert
+        var ajax1 = fillMyPersonalBox()
+        var ajax2;
+        // Warten bis mypersonalbox fertig geladen
+        $.when(ajax1).done(function(){
+            // Pruefe ob der Benutzer immernoch eingeloggt ist
+        	if(jsonBenutzer == undefined) // Der Benutzer ist nicht mehr eingeloggt
+    		{
+        	    // Lade die Startseite und gehe dort hin, um erneuten Login zu ermoeglichen
+                $.when(fillStartseite()).done(function() {
+                    console.log("1");
+                	display(ansichtStartseite);
+        		});
+                return;
+    		}
+        	else if(ziel == undefined ||                    // Wenn kein location Parameter oder
+	           $.inArray(ziel, alleAnsichten) == -1 ||      // ein bekannter location Parameter oder
+	           ziel == ansichtStartseite ||                 // location ist Startseite oder
+	           ziel == ansichtHauptseite)                   // location ist Hauptseite
+	        {
+	            ziel = ansichtHauptseite;                   // Dann gehe zu Hauptseite
+	            ajax2 = fillHauptseite();
+	        }
+	        else if(ziel == ansichtProfilseite) 
+	        {
+	            // Stosse das Laden der Profilseite an
+	        	ajax2 = fillProfilseite();
+	        }
+	        else if(ziel == ansichtVeranstaltungsseite) {
+	            // Bei der Veranstaltungsseite muss zum Laden die vnID gegeben sein
+	            // und es kann eine kkID gegeben sein, falls zu einer bestimmen KK gesprungen werden soll
+	        	var vid = paramObj[paramId];
+	        	var kkId = paramObj[paramURLKkID];
+	        	ajax2 = fillVeranstaltungsSeite(vid, kkId);
+	        	// Wenn Veranstaltungsseite komplett geladen, aktiviere einfach alle Waypoints
+	        	// (es werden momentan sowieso alle Waypoints dort erstellt)
+	        	ajax2.done(function(){
+	        		Waypoint.enableAll();
+	        	});
+	        }
+	        $.when(ajax2).done(function() {
+                console.log("2");
+	        	display(ziel);
+			});
+        });
+        // Individuell eingestelltes Theme setzen
         if(jsonBenutzer[paramTheme] == "DAY")
         {
             changeCSS("CSS/sopra_light.css", 0);
@@ -206,73 +175,19 @@ function interpreteUrlQuery(paramObj)
             changeCSS("CSS/sopra.css", 0);
             changeCSS("CSS/mybuttonstyle.css", 1);
         }
-        
-        var ajax1 = fillMyPersonalBox()
-        var ajax2;
-        // Warten auf das füllen der Personal Box
-        $.when(ajax1).done(function(){
-        	
-        	// Benutzer immernoch gültig?
-        	if(jsonBenutzer == undefined)
-    		{
-                $.when(fillStartseite()).done(function() {
-                	display(ansichtStartseite);
-        		});
-                return;
-    		}
-        	
-        	else if(ziel == undefined ||                         // Kein location Parameter
-	           $.inArray(ziel, alleAnsichten) == -1 ||      // Kein bekannter location Parameter
-	           ziel == ansichtStartseite ||                 // location ist Startseite
-	           ziel == ansichtHauptseite)                   // location ist Hauptseite
-	        {
-	            ziel = ansichtHauptseite;                   // Dann gehe zu Hauptseite
-	            ajax2 = fillHauptseite();
-	        }
-	        else if(ziel == ansichtProfilseite) 
-	        {
-	        	ajax2 = fillProfilseite();
-	        }
-	        else if(ziel == ansichtVeranstaltungsseite){
-	        	vid = paramObj[paramId];
-	        	kkId = paramObj[paramURLKkID];
-	        	ajax2 = fillVeranstaltungsSeite(vid, kkId);
-	        	ajax2.done(function(){
-	        		Waypoint.enableAll();
-	        	});
-	        }
-	        
-	        $.when(ajax2).done(function() {
-	        	display(ziel);
-			});
-        });
     } 
     // Benutzer nicht eingeloggt
     else 
     {
         $.when(fillStartseite()).done(function() {
+            console.log("3");
         	display(ansichtStartseite);
 		});
     }
-}
-
-/**
- * Enthaelt den Ajax Call mit der getBenutzer action.
- * @returns when Ajax Call is finished
- */
-function getBenutzer()
-{
-    return ajaxCall(
-        profilServlet,
-        actionGetBenutzer,
-        function(response) {
-            jsonBenutzer = response;
-        },
-        undefined,
-        function(errCode) {
-            jsonBenutzer = undefined;
-        }
-    );
+    // Alle Waypoints loeschen, da diese in der Veranstaltungsseite neu erstellt werden
+    Waypoint.destroyAll();
+    // Check ob in der Mobile-Ansicht andere Buttons angezeigt werden muessen
+    onResizeHandler();
 }
 
 /**
@@ -312,6 +227,7 @@ function display(ansicht)
     
     return $.Deferred().resolve();
 }
+
 /**
  * Diese Funktion setzt die URL und wechselt zum angegebenen Profil
  * @param benutzerID
@@ -323,6 +239,7 @@ function gotoProfil(benutzerID)
 	paramObj[urlParamId] = benutzerID;
 	buildUrlQuery(paramObj);
 }
+
 /**
  * Diese Funktion setzt die URL und wechselt zur angegebenen Veranstaltung
  * @param veranstId
@@ -336,6 +253,7 @@ function gotoVeranstaltung(veranstId, kkId)
 		paramObj[paramURLKkID] = kkId;
 	buildUrlQuery(paramObj);
 }
+
 /**
  * Diese Funktion setzt die URL und wechselt zur Hautseite
  */
@@ -354,4 +272,94 @@ function gotoStartseite()
 	var paramObj = {};
 	paramObj[urlParamLocation] = ansichtStartseite;
 	buildUrlQuery(paramObj);
+}
+
+/**
+ * Liefert den Wert zu einem gegebenen Parameternamen.
+ * @see http://stackoverflow.com/questions/901115
+ * @param name ist der Parametername
+ * @returns Wert des Parameters
+ * oder leerer String bei nicht vorhandenem Parameter
+ */
+function getUrlParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+/**
+ * Holt den aktuell eingloggten Benutzer vom Server und speichert ihn in jsonBenutzer
+ * @returns Ajax Objekt
+ */
+function getBenutzer()
+{
+    return ajaxCall(
+        profilServlet,
+        actionGetBenutzer,
+        function(response) {
+            jsonBenutzer = response;
+        },
+        undefined,
+        function(errCode) {
+            jsonBenutzer = undefined;
+        }
+    );
+}
+
+$( window ).resize(function() {    
+    onResizeHandler();
+});
+
+/**
+ * Wird bei Groessenaenderung des Fensters sowie bei Seitenwechseln aufgerufen.
+ * Blendet kontextsensitive Buttons in der Ansicht fuer kleine Bildschirme ein bzw. aus.
+ */
+function onResizeHandler() 
+{
+    // Elemente fuer kleine Bildschirme
+    if (window.matchMedia("(max-width: 56em)").matches)
+    {
+        if(window.location.href.indexOf("hauptseite")>=0){
+            $(".r-suche_etwas_label").show();
+            $(".r-kk-inhaltsvz-toggle").hide();
+            $(".r-kk-inhaltsvz-toggle-screensize-m").hide();
+        }
+        else if(window.location.href.indexOf("veranstaltungsseite")>=0){
+            $(".r-kk-inhaltsvz-toggle-screensize-m").removeAttr("style");
+            $(".r-kk-inhaltsvz-toggle").removeAttr("style");
+            $(".r-suche_etwas_label").hide();
+        }
+        else
+        {
+            $(".r-suche_etwas_label").hide();
+            $(".r-kk-inhaltsvz-toggle").hide();
+            $(".r-kk-inhaltsvz-toggle-screensize-m").hide();
+        }
+    }
+    else if (window.matchMedia("(max-width: 80em)").matches)
+    {
+        if(window.location.href.indexOf("hauptseite")>=0){
+            $(".r-suche_etwas_label").hide();
+            $(".r-kk-inhaltsvz-toggle").hide();
+            $(".r-kk-inhaltsvz-toggle-screensize-m").hide();
+        }
+        else if(window.location.href.indexOf("veranstaltungsseite")>=0){
+            $(".r-kk-inhaltsvz-toggle-screensize-m").removeAttr("style");
+            $(".r-kk-inhaltsvz-toggle").hide();
+            $(".r-suche_etwas_label").hide();
+        }
+        else
+        {
+            $(".r-suche_etwas_label").hide();
+            $(".r-kk-inhaltsvz-toggle").hide();
+            $(".r-kk-inhaltsvz-toggle-screensize-m").hide();
+        }
+    }
+    else
+    {
+        $(".r-suche_etwas_label").hide();
+        $(".r-kk-inhaltsvz-toggle").hide();
+        $(".r-kk-inhaltsvz-toggle-screensize-m").hide();
+    }
 }
